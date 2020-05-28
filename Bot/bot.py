d2b0810a27cc697f58488806507716a351962129
sairam4123 <opencity@info.github.com>
2020-05-28 20:18:13 +0530

    Suggestions Update for OpenCityBot_PostgresSQL 1.28.0

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,17 +1,24 @@
+import datetime
 import os
 
 import asyncpg
 import discord
 from discord.ext import commands, tasks
+from quart import Quart
 
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+CLIENT_ID = os.getenv('DISCORD_CLIENT_ID')
+
 BOT_IS_READY = False
+PREFIX = os.getenv('DEFAULT_PREFIX')
+DELIMITER = os.getenv('DEFAULT_DELIMITER_FOR_ENV')
+TICKET_EMOJI = os.getenv('DEFAULT_TICKET_EMOJI')
 
 
 async def get_prefix(bot_1, message):
     if message.channel.type == discord.ChannelType.private:
-        return commands.when_mentioned_or(['i!', 'i?'])(bot_1, message)
+        return commands.when_mentioned_or(*bot_1.prefix_default)(bot_1, message)
     prefixes = await bot_1.pg_conn.fetchrow("""
     SELECT prefixes FROM prefix_data
     WHERE guild_id = $1
@@ -20,13 +27,26 @@ async def get_prefix(bot_1, message):
         await bot_1.pg_conn.execute("""
         INSERT INTO prefix_data (guild_id, prefixes)
         VALUES ($1, $2)
-        """, message.guild.id, ["i!", "i?"])
-        return commands.when_mentioned_or(*['i!', 'i?'])(bot_1, message)
+        """, message.guild.id, bot_1.prefix_default)
+        return commands.when_mentioned_or(*bot_1.prefix_default)(bot_1, message)
     return commands.when_mentioned_or(*prefixes[0])(bot_1, message)
 
 
 bot = commands.Bot(command_prefix=get_prefix)
-init_cogs = [f'Bot.cogs.{filename[:-3]}' for filename in os.listdir('Bot/cogs') if filename.endswith('.py')]
+app = Quart(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello from {}".format(bot.user.name)
+
+
+bot.oauth_url = discord.utils.oauth_url(client_id=CLIENT_ID, permissions=discord.Permissions(8))
+bot.init_cogs = [f'Bot.cogs.{filename[:-3]}' for filename in os.listdir('Bot/cogs') if filename.endswith('.py')]
+bot.start_time = datetime.datetime.utcnow()
+bot.prefix_default = PREFIX.split(DELIMITER)
+bot.start_number = 1000000000000000
+bot.ticket_emoji_default = TICKET_EMOJI.split(DELIMITER)
 
 
 async def connection_for_pg():
@@ -72,7 +92,7 @@ async def add_guild_to_db():
                 await bot.pg_conn.execute("""
                 INSERT INTO cogs_data (guild_id, enabled, disabled)
                 VALUES ($1, $2, $3)
-                """, guild.id, init_cogs, ["None"])
+                """, guild.id, bot.init_cogs, ["None"])
 
 
 # @add_guild_to_db.error
--- Bot/cogs/Suggestions.py
+++ Bot/cogs/Suggestions.py
@@ -1,4 +1,4 @@
-import json
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -11,10 +11,10 @@ class Suggestions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def cog_check(self, ctx):
+    async def cog_check(self, ctx):
         if ctx.channel.type == discord.ChannelType.private:
             return True
-        enabled = self.bot.pg_conn.fetchrow("""
+        enabled = await self.bot.pg_conn.fetchrow("""
          SELECT enabled FROM cogs_data
          WHERE guild_id = $1
          """, ctx.guild.id)
@@ -22,55 +22,186 @@ class Suggestions(commands.Cog):
             return True
         return False
 
+    @commands.group(help="Does nothing when invoked without subcommand.")
+    async def suggestion(self, ctx):
+        pass
+
     @commands.group(help="Suggest something!", invoke_without_command=True, usage="<type_of_suggestion> <suggestion>")
-    async def suggest(self, ctx: commands.Context, type1, *, suggestion):
-        counts = await self.bot.pg_conn.fetch("""
-        SELECT * FROM id_data
+    async def suggest(self, ctx: commands.Context, type1, *, suggestion: str):
+        suggestion_id = await self.bot.pg_conn.fetchval("""
+        SELECT suggestion_id FROM id_data
         """)
-        if "suggestion_id" not in counts:
-            counts["id"] = {}
-        # if str(ctx.guild.id) not in counts.keys():
-        #     counts[str(ctx.guild.id)] = {}
-        # if "suggestion_id" not in counts["id"].keys():
-        #     counts["id"]["suggestion_id"] = self.bot.start_number
-        # if "suggestion_number" not in counts[str(ctx.guild.id)].keys():
-        #     counts[str(ctx.guild.id)]["suggestion_number"] = 1
-        # title = f"Suggestion #{counts[str(ctx.guild.id)]['suggestion_number']}"
-        # embed = discord.Embed(
-        #     title=title,
-        #     description=(
-        #         f"**Suggestion**: {suggestion}\n"
-        #         f"**Suggestion by**: {ctx.author.mention}"
-        #     ),
-        #     color=discord.Colour.green()
-        # ).set_footer(text=f"SuggestionID: {counts['id']['suggestion_id']} | {indian_standard_time_now()[1]}")
-        # embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
-        # message_sent = await ctx.send(embed=embed)
-        # await message_sent.add_reaction(f":_tick:705003237174018179")
-        # await message_sent.add_reaction(f":_neutral:705003236687609936")
-        # await message_sent.add_reaction(f":_cross:705003237174018158")
-        # await message_sent.add_reaction(f":_already_there:705003236897194004")
-        # suggestions = json.load(open(self.bot.suggestions_json))
-        # if "suggestions" not in suggestions.keys():
-        #     suggestions["suggestions"] = []
-        # suggestion_1 = {
-        #     "suggestionID": counts['id']["suggestion_id"],
-        #     "suggestionMessageID": message_sent.id,
-        #     "suggestionTitle": title,
-        #     "suggestionContent": suggestion,
-        #     "suggestionAuthor": f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})",
-        #     "suggestionTime": indian_standard_time_now()[1],
-        #     "suggestionChannelID": message_sent.channel.id,
-        #     "suggestionGuildID": message_sent.guild.id,
-        #     "suggestionType": f"{type1}",
-        #     "suggestionStatus": "waiting"
-        # }
-        # counts['id']["suggestion_id"] += 1
-        # counts[str(ctx.guild.id)]["suggestion_number"] += 1
-        # json.dump(counts, open(self.bot.counts_json, "w+"), indent='\t')
-        # suggestions["suggestions"].append(suggestion_1)
-        # json.dump(suggestions, open(self.bot.suggestions_json, "w+"), indent='\t')
-        # await ctx.author.send("Your suggestion is sent!, This is how your suggestion look like!", embed=embed)
+        suggestion_number = await self.bot.pg_conn.fetchval("""
+        SELECT suggestion_number FROM count_data
+        WHERE guild_id = $1
+        """, ctx.guild.id)
+        if not suggestion_id:
+            await self.bot.pg_conn.execute("""
+            INSERT INTO id_data (suggestion_id)
+            VALUES ($1)
+            """, self.bot.start_number)
+            await ctx.reinvoke()
+        if not suggestion_number:
+            await self.bot.pg_conn.execute("""
+            INSERT INTO count_data (guild_id, suggestion_number)
+            VALUES ($1, $2)
+            """, ctx.guild.id, 1)
+            await ctx.reinvoke()
+        title = f"Suggestion #{suggestion_number}"
+        embed = discord.Embed(
+            title=title,
+            description=(
+                f"**Suggestion**: {suggestion}\n"
+                f"**Suggestion by**: {ctx.author.mention}"
+            ),
+            color=discord.Colour.green()
+        ).set_footer(text=f"SuggestionID: {suggestion_id} | {indian_standard_time_now()[1]}")
+        embed.set_author(name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}")
+        message_sent = await ctx.send(embed=embed)
+        await message_sent.add_reaction(f":_tick:705003237174018179")
+        await message_sent.add_reaction(f":_neutral:705003236687609936")
+        await message_sent.add_reaction(f":_cross:705003237174018158")
+        await message_sent.add_reaction(f":_already_there:705003236897194004")
+        await self.bot.pg_conn.execute("""
+        INSERT INTO suggestion_data
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+        """, suggestion_id, message_sent.id, title, suggestion, f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})", indian_standard_time_now()[1],
+                                       message_sent.channel.id, message_sent.guild.id, type1, "waiting", "Null")
+        await self.bot.pg_conn.execute("""
+        UPDATE id_data
+        SET suggestion_id = $1
+        WHERE row_id = 0
+        """, int(suggestion_id) + 1)
+        await self.bot.pg_conn.execute("""
+        UPDATE count_data
+        SET suggestion_number = $1
+        WHERE guild_id = $2
+        """, int(suggestion_number) + 1, ctx.guild.id)
+        await ctx.author.send("Your suggestion is sent!, This is how your suggestion look like!", embed=embed)
+
+    @suggestion.command(name="approve", help="Approves a suggestion.")
+    async def suggestion_approve(self, ctx: commands.Context, suggestion_id: int, *, reason: Optional[str] = None):
+        suggestion = [await self.bot.pg_conn.fetchrow("""
+        SELECT * FROM suggestion_data
+        WHERE "suggestionID" = $1
+        """, suggestion_id)]
+        embed = discord.Embed()
+        author = ctx.guild.get_member(int(suggestion[0]['suggestionAuthor'].split(' ')[-1].strip('( )')))
+        suggestion_moderator = f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
+        embed.title = suggestion[0]['suggestionTitle'] + " Approved"
+        embed.description = (
+                    f"**Suggestion**: {suggestion[0]['suggestionContent']}\n"
+                    f"**Suggested by**: {author.mention}"
+        )
+        if reason:
+            embed.add_field(name=f"Reason by {f'{ctx.author.name}#{ctx.author.discriminator}'}", value=reason)
+        embed.set_author(name=author.name, icon_url=author.avatar_url)
+        embed.colour = discord.Colour.dark_green()
+        embed.set_footer(text=f"SuggestionID: {suggestion[0]['suggestionID']} | {suggestion[0]['suggestionTime']}")
+        suggestion_guild = self.bot.get_guild(suggestion[0]['suggestionGuildID'])
+        suggestion_channel = suggestion_guild.get_channel(suggestion[0]['suggestionChannelID'])
+        suggestion_message = await suggestion_channel.fetch_message(suggestion[0]["suggestionMessageID"])
+        await suggestion_message.edit(embed=embed)
+        await self.bot.pg_conn.execute("""
+        UPDATE suggestion_data 
+        SET "suggestionStatus" = $2, 
+        "suggestionModerator" = $3
+        WHERE "suggestionID" = $1
+        """, suggestion_id, "approved", suggestion_moderator)
+        await ctx.send(f"Approved suggestion {suggestion_id} because of {reason}")
+
+    @suggestion.command(name="deny", help="Denies a suggestion.")
+    async def suggestion_deny(self, ctx: commands.Context, suggestion_id: int, *, reason: Optional[str] = None):
+        suggestion = [self.bot.pg_conn.execute("""
+                SELECT * FROM suggestion_data
+                WHERE "suggestionID" = $1
+                """, suggestion_id)]
+        embed = discord.Embed()
+        author = ctx.guild.get_member(int(suggestion[0]['suggestionAuthor'].split(' ')[-1].strip('( )')))
+        suggestion_moderator = f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
+        embed.title = suggestion[0]['suggestionTitle'] + " Denied"
+        embed.description = (
+            f"**Suggestion**: {suggestion[0]['suggestionContent']}\n"
+            f"**Suggested by**: {author.mention}"
+        )
+        if reason:
+            embed.add_field(name=f"Reason by {f'{ctx.author.name}#{ctx.author.discriminator}'}", value=reason)
+        embed.set_author(name=author.name, icon_url=author.avatar_url)
+        embed.colour = discord.Colour.blue()
+        embed.set_footer(text=f"SuggestionID: {suggestion[0]['suggestionID']} | {suggestion[0]['suggestionTime']}")
+        suggestion_guild = self.bot.get_guild(suggestion[0]['suggestionGuildID'])
+        suggestion_channel = suggestion_guild.get_channel(suggestion[0]['suggestionChannelID'])
+        suggestion_message = await suggestion_channel.fetch_message(suggestion[0]["suggestionMessageID"])
+        await suggestion_message.edit(embed=embed)
+        await self.bot.pg_conn.execute("""
+        UPDATE suggestion_data 
+        SET "suggestionStatus" = $2, 
+        "suggestionModerator" = $3
+        WHERE "suggestionID" = $1
+        """, suggestion_id, "denied", suggestion_moderator)
+        await ctx.send(f"Denied suggestion {suggestion_id} because of {reason}")
+
+    @suggest.command(name="consider", help="Marks a suggestion as considered.")
+    async def suggestion_consider(self, ctx: commands.Context, suggestion_id: int, *, reason: Optional[str] = None):
+        suggestion = [self.bot.pg_conn.execute("""
+                    SELECT * FROM suggestion_data
+                    WHERE "suggestionID" = $1
+                    """, suggestion_id)]
+        embed = discord.Embed()
+        author = ctx.guild.get_member(int(suggestion[0]['suggestionAuthor'].split(' ')[-1].strip('( )')))
+        suggestion_moderator = f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
+        embed.title = suggestion[0]['suggestionTitle'] + " Considered"
+        embed.description = (
+                f"**Suggestion**: {suggestion[0]['suggestionContent']}\n"
+                f"**Suggested by**: {author.mention}"
+         )
+        if reason:
+            embed.add_field(name=f"Reason by {f'{ctx.author.name}#{ctx.author.discriminator}'}", value=reason)
+        embed.set_author(name=author.name, icon_url=author.avatar_url)
+        embed.colour = discord.Colour.dark_red()
+        embed.set_footer(text=f"SuggestionID: {suggestion[0]['suggestionID']} | {suggestion[0]['suggestionTime']}")
+        suggestion_guild = self.bot.get_guild(suggestion[0]['suggestionGuildID'])
+        suggestion_channel = suggestion_guild.get_channel(suggestion[0]['suggestionChannelID'])
+        suggestion_message = await suggestion_channel.fetch_message(suggestion[0]["suggestionMessageID"])
+        await suggestion_message.edit(embed=embed)
+        await self.bot.pg_conn.execute("""
+            UPDATE suggestion_data 
+            SET "suggestionStatus" = $2, 
+            "suggestionModerator" = $3
+            WHERE "suggestionID" = $1
+        """, suggestion_id, "considered", suggestion_moderator)
+        await ctx.send(f"Marked suggestion {suggestion_id} as considered because of {reason}")
+
+    @suggestion.command(name="implemented", help="Marks a suggestion as already implemented.")
+    async def suggestion_implemented(self, ctx: commands.Context, suggestion_id: int, *, reason: Optional[str] = None):
+        suggestion = [await self.bot.pg_conn.fetchrow("""
+               SELECT * FROM suggestion_data
+               WHERE "suggestionID" = $1
+               """, suggestion_id)]
+        embed = discord.Embed()
+        author = ctx.guild.get_member(int(suggestion[0]['suggestionAuthor'].split(' ')[-1].strip('( )')))
+        suggestion_moderator = f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
+        embed.title = suggestion[0]['suggestionTitle'] + " Implemented"
+        embed.description = (
+            f"**Suggestion**: {suggestion[0]['suggestionContent']}\n"
+            f"**Suggested by**: {author.mention}"
+        )
+        if reason:
+            embed.add_field(name=f"Reason by {f'{ctx.author.name}#{ctx.author.discriminator}'}", value=reason)
+        embed.set_author(name=author.name, icon_url=author.avatar_url)
+        embed.colour = discord.Colour.dark_green()
+        embed.set_footer(text=f"SuggestionID: {suggestion[0]['suggestionID']} | {suggestion[0]['suggestionTime']}")
+        suggestion_guild = self.bot.get_guild(suggestion[0]['suggestionGuildID'])
+        suggestion_channel = suggestion_guild.get_channel(suggestion[0]['suggestionChannelID'])
+        suggestion_message = await suggestion_channel.fetch_message(suggestion[0]["suggestionMessageID"])
+        await suggestion_message.edit(embed=embed)
+        await self.bot.pg_conn.execute("""
+               UPDATE suggestion_data 
+               SET "suggestionStatus" = $2, 
+               "suggestionModerator" = $3
+               WHERE "suggestionID" = $1
+               """, suggestion_id, "implemented", suggestion_moderator)
+        await ctx.send(f"Marked suggestion {suggestion_id} as already implemented because of {reason}")
 
 
 def setup(bot):
