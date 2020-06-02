d4d1b364bbeab31a6be8ee2a75ac33859b764047
sairam4123 <opencity@info.github.com>
2020-06-02 09:23:54 +0530

    Update for OpenCityBot_PostgresSQL 1.34.1
    + Added member_leave and member_kick events in dispatcher.py.

--- Bot/cogs/utils/dispatcher.py
+++ Bot/cogs/utils/dispatcher.py
@@ -1,3 +1,5 @@
+import datetime
+
 import discord
 from discord.ext import commands
 
@@ -6,6 +8,17 @@ class dispatcher(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        async for entry in member.guild.audit_logs(action=discord.AuditLogAction.kick):
+            if (datetime.datetime.utcnow() - entry.created_at).total_seconds() < 20:
+                if entry.target == member:
+                    self.bot.dispatch('member_kick', member)
+                    return
+        if member.guild.fetch_ban(member):
+            return
+        self.bot.dispatch('member_leave', member)
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
