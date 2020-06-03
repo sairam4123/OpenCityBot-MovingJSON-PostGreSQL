ccee0df21176c6dc9793416290e42849ca680e59
sairam4123 <opencity@info.github.com>
2020-06-03 12:02:51 +0530

    Hotfix Update for OpenCityBot_PostgresSQL 1.36.1
    * Fixed a minor bug in Leveling

--- Bot/cogs/Leveling.py
+++ Bot/cogs/Leveling.py
@@ -378,7 +378,7 @@ class Leveling(commands.Cog):
     @commands.check_any(is_guild_owner(), commands.is_owner())
     async def level_remove(self, ctx: commands.Context, member: Optional[discord.Member] = None, level=0):
         member = ctx.author if member is None else member
-        old_level = await self.get_xps(member)
+        old_level = await self.get_level(member)
         await self.set_level(member, int(int(old_level) - int(level)))
         await ctx.send(f"Removed level {level} to {member.mention}")
 
