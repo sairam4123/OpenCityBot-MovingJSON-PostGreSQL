12c08e71452f251af084e51f53465815106467e0
sairam4123 <opencity@info.github.com>
2020-06-05 09:21:06 +0530

    Update for OpenCityBot_PostgresSQL 1.38.0
    + Added Join_To_Create_Voice_Channel

---
+++ Bot/cogs/utils/numbers.py
@@ -0,0 +1,14 @@
+def make_ordinal(n):
+    """
+    Convert an integer into its ordinal representation::
+
+        make_ordinal(0)   => '0th'
+        make_ordinal(3)   => '3rd'
+        make_ordinal(122) => '122nd'
+        make_ordinal(213) => '213th'
+    """
+    n = int(n)
+    suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+    if 11 <= (n % 100) <= 13:
+        suffix = 'th'
+    return str(n) + suffix
---
+++ Bot/cogs/utils/timeformat_bot.py
@@ -0,0 +1,20 @@
+import datetime
+
+
+def indian_standard_time_now():
+    ist_date_obj = datetime.timedelta(minutes=30, hours=5)
+    ist_time = datetime.datetime.utcnow() + ist_date_obj
+    format_time = ist_time.strftime("%a, %#d %B %Y %#I:%M %p")
+    return ist_time, format_time, ist_time.timestamp()
+
+
+def convert_utc_into_ist(utc_date: datetime.datetime):
+    ist_date_obj = datetime.timedelta(minutes=30, hours=5)
+    ist_time = utc_date + ist_date_obj
+    format_time = ist_time.strftime("%a, %#d %B %Y %#I:%M %p")
+    return ist_time, format_time, ist_time.timestamp()
+
+
+if __name__ == '__main__':
+    print(indian_standard_time_now())
+    print(convert_utc_into_ist(utc_date=datetime.datetime.utcnow()))
