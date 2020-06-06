cd8aaf8c0db9dbd67d9b885c48607cd605144c53
sairam4123 <opencity@info.github.com>
2020-06-06 22:48:45 +0530

    Update for OpenCityBot_PostgresSQL 1.39.2
    + Added a random status between animated and non animated status.
    
    + Added avatar command.
    
    + Added emoji_info command.

--- Bot/cogs/utils/status_discord.py
+++ Bot/cogs/utils/status_discord.py
@@ -1,15 +1,17 @@
+import random
+
 import discord
 
 
 def get_status_of(member: discord.Member) -> str:
     if member.status == discord.Status.idle:
-        status = "<:idle:713029270976331797> Idle"
+        status = random.choice(["<:idle:713029270976331797> Idle", "<a:idle1:718510652616081408> Idle"])
     elif member.status == discord.Status.do_not_disturb:
-        status = "<:dnd:713029270489792533> Do Not Disturb"
+        status = random.choice(["<:dnd:713029270489792533> Do Not Disturb", "<a:dnd1:718510652670607360> Do Not Disturb"])
     elif member.status == discord.Status.online:
-        status = "<:online:713029272125833337> Online"
+        status = random.choice(["<:online:713029272125833337> Online", "<a:online1:718510653186244636> Online"])
     elif member.status == discord.Status.offline:
-        status = "<:invisible:713029271391830109> Offline"
+        status = random.choice(["<:invisible:713029271391830109> Offline", "<a:invisible1:718510652674801675> Offline"])
     else:
-        status = "<:invisible:713029271391830109> Invisible"
+        status = random.choice(["<:invisible:713029271391830109> Invisible", "<a:invisible1:718510652674801675> Invisible"])
     return status
