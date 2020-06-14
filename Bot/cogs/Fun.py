b2b180475a63b33b40e706debca95a1d7216ecac
sairam4123 <opencity@info.github.com>
2020-06-14 14:09:28 +0530

    Update for OpenCityBot_PostgresSQL 1.40.2
    * Reformatted Fun.py to keep with PEP-8.

--- Bot/cogs/Fun.py
+++ Bot/cogs/Fun.py
@@ -1,9 +1,12 @@
-import discord
-from discord.ext import commands
 import asyncio
 import random
+
+import discord
+from discord.ext import commands
+
+
 class Fun(commands.Cog):
-    
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -20,54 +23,49 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def joke_add(self, ctx, *, question):
-        try:
-            db = await self.bot.pg_conn.fetch("SELECT * FROM jokes_data")
+        if question:
+            await ctx.send("Good. Now enter the answer")
 
-            if question:
-                await ctx.send("Good. Now enter the answer")
+            def check(m):
+                return m.author == ctx.author
 
-                def check(m):
-                    return m.author == ctx.author
-
+            try:
                 ans = await self.bot.wait_for('message', check=check, timeout=30)
                 if ans:
-
                     await ctx.send("Added the question :thumbsup:")
+                    await self.bot.pg_conn.execute("INSERT INTO jokes_data(questions,answers) VALUES($1,$2)", question, ans.content)
                 else:
                     await ctx.send("Please enter the answer")
-            else:
-                await ctx.send("Please enter the question")
-            await self.bot.pg_conn.execute("INSERT INTO jokes_data(questions,answers) VALUES($1,$2)", question, ans.content)
-
-        except asyncio.TimeoutError:
-            await ctx.send("Oops, time up!:clock: Try again if you want to add your question")
-
+                # await self.bot.pg_conn.execute("INSERT INTO jokes_data(questions,answers) VALUES($1,$2)", question, ans.content)
+            except asyncio.TimeoutError:
+                await ctx.send("Oops, time up!:clock: Try again if you want to add your question")
         else:
-            raise
+            await ctx.send("Please enter the question")
 
     @joke_add.error
-    async def jokeadd_error(self,ctx, e):
-        if isinstance(e, commands.MissingRequiredArgument):
+    async def joke_add_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please enter the question along with the command like **$joke_add Yo Mama ...**")
         else:
             raise
 
+    def random_between_cycle_and_random(self, list_1):
+        pass
+
     @commands.command()
-    async def jokes(self,ctx):
-        already = []
-        db = await self.bot.pg_conn.fetch("SELECT questions,answers FROM jokes_data")
-        jok = random.choice(db)
-        ques = jok['questions']
-        answ = jok['answers']
-        while ques in  already:
-            jok = random.choice(db)
-            ques = jok['questions']
-            answ = jok['answers']
-        already.append(ques)
-        jk = discord.Embed(title = "J:laughing:ke",description = ques,color = 0x0E2FE5)
-        await ctx.send(embed = jk)
+    async def jokes(self, ctx):
+        jokes_data = await self.bot.pg_conn.fetch("""
+        SELECT questions, answers FROM jokes_data
+        """)
+        joke = random.choice(jokes_data)
+        question = joke['questions']
+        answer = joke['answers']
+        joke_embed = discord.Embed(title="J:laughing:ke", description=question, color=0x0E2FE5)
+        await ctx.send(embed=joke_embed)
         await asyncio.sleep(5)
-        ans = discord.Embed(description = answ,color = 0x0E2FE5)
-        await ctx.send(embed = ans)
+        ans = discord.Embed(description=answer, color=0x0E2FE5)
+        await ctx.send(embed=ans)
+
+
 def setup(bot):
     bot.add_cog(Fun(bot))
